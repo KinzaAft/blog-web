54755145397b26099ce501557ddf948a878581b1
Kinza Aftab <160029911+KinzaAft@users.noreply.github.com>
2024-10-09 00:09:30 +0500

    Update header.tsx

--- Components/header.tsx
+++ Components/header.tsx
@@ -4,9 +4,9 @@ export default function Header(){
         <div className="nav-container">
             <h1 className="logo">Travel <span>Blog</span><span className="logo-2"> Blog Website</span></h1>
         <ul>
-             <li><Link href="#second-container">Home</Link></li>
-             <li><Link href="/about">About</Link></li>
-             <li><Link href="/contact">Contact</Link></li>
+             <li>Home</li>
+             <li>About</li>
+             <li>Contact</li>
         </ul>
         <svg id="bars" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color={"#000000"} fill={"none"}>
         <path d="M4 5L20 5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
